1f4957d13346026bd61741f8d2c7f3e009d4b064
Daniel Pereira Dos Santos Costa <57815094+Danie4685@users.noreply.github.com>
2022-11-13 23:51:15 -0300

    Add files via upload

--- application/views/home.php
+++ application/views/home.php
@@ -5,8 +5,9 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Hello, Bootstrap Table!</title>
+  <title>Empresa</title>
 
+  <script src="https://kit.fontawesome.com/775fd40529.js" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="//code.jquery.com/jquery-3.6.1.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
@@ -26,6 +27,7 @@
           $('#PrimeiraParte').removeClass('d-none');
           $('#SegundaParte').addClass('d-none');
           swal("Senha invalida!");
+          swal("Atenção!", "Login ou senha esta incorreta!", "info");
 
 
         }
@@ -36,71 +38,83 @@
       var retorno = [{
           "nome": 'Daniel',
           "idade": '25',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de sistemas'
         },
         {
           "nome": 'Bruno',
           "idade": '21',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de suporte'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Bruno',
           "idade": '22',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Vinicius',
           "idade": '19',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Fernando',
           "idade": '19',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
 
         {
           "nome": 'Mauricio',
           "idade": '28',
+          "cpf": '000-000-000-00',
           "Prof": 'Analista de infraestrutura'
         },
       ]
@@ -110,16 +124,17 @@
 
     }
 
-    function teste(value, row) {
-      return '<button class="btn btn-danger" onclick="Editar(\'' + value + '\',\'' + row.idade + '\',\'' + row.Prof + '\')"> Consultar </button>';
+    function EditarItem(value, row) {
+      return '<button class="btn btn-primary" onclick="Editar(\'' + value + '\',\'' + row.idade + '\',\'' + row.Prof + '\',\'' + row.cpf + '\')"> <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar </button>';
     }
 
-    function Editar(value, idade, professor) {
+    function Editar(value, idade, professor,cpf) {
       $('#exampleModalLong').modal('show');
 
       $('#nome').val(value);
       $('#idade').val(idade);
       $('#profissao').val(professor);
+      $('#cpf').val(cpf);
     }
 
     function fechar() {
@@ -141,6 +156,10 @@
       })
       return html.join('')
     }
+
+    function SalvarEdit() {
+      swal("OK!", "Salvo com sucesso!", "success");
+    }
   </script>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -148,7 +167,7 @@
   <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.css">
 </head>
 
-<body style="background: #7f8083;">
+<body style="background-image: url('application/controllers/imagem.jpg')">
   <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="#">Empresa</a>
   </nav>
@@ -183,13 +202,6 @@
     </div>
   </div>
 
-
-
-
-
-
-
-
   <div id="SegundaParte" class="d-none">
 
     <div class="container">
@@ -199,8 +211,9 @@
             <tr>
               <th data-field="nome" data-align="center">Nome</th>
               <th data-field="idade" data-align="center">Idade</th>
+              <th data-field="cpf" data-align="center">CPF</th>
               <th data-field="Prof" data-align="center">Profissão</th>
-              <th data-field="nome" data-align="center" data-formatter="teste">Editar</th>
+              <th data-field="nome" data-align="center" data-formatter="EditarItem">Editar</th>
             </tr>
           </thead>
         </table>
@@ -209,7 +222,7 @@
   </div>
   <!-- Modal -->
   <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLongTitle">FUNCIONARIO</h5>
@@ -225,13 +238,17 @@
             <div class="col-2">
               <input type="text" class="form-control" id="idade">
             </div>
-            <div class="col-6">
+            <div class="col-3">
+              <input type="text" class="form-control" id="cpf">
+            </div>
+            <div class="col-3">
               <input type="text" class="form-control" id="profissao">
             </div>
           </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" onclick="fechar()" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success" onclick="SalvarEdit()" data-dismiss="modal">Salvar</button>
         </div>
       </div>
     </div>
