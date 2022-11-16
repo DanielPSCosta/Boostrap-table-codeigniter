bc90419c158ed36d63a3ed1cc0206c4c18f90c54
Daniel Pereira Dos Santos Costa <57815094+Danie4685@users.noreply.github.com>
2022-11-16 08:53:13 -0300

    Add files via upload

--- application/models/M_busca.php
+++ application/models/M_busca.php
@@ -14,4 +14,65 @@ class M_busca extends CI_Model
             return 2;
         }
     }
+
+    public function busca()
+    {
+        $retorno = [
+            [
+                "nome" => "Daniel",
+                "idade" => "25",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Eliel",
+                "idade" => "21",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Mauricio",
+                "idade" => "28",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Marcio",
+                "idade" => "39",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Andre",
+                "idade" => "23",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Bras",
+                "idade" => "60",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Bruno",
+                "idade" => "20",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Vinicius",
+                "idade" => "19",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ],
+            [
+                "nome" => "Fernando",
+                "idade" => "19",
+                "cpf" => "000-000-000-00",
+                "Prof" => "Analista",
+            ]
+        ];
+        return $retorno;
+    }
 }
